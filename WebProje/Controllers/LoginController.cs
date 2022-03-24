f5a7312f6686558bf488021c83f164bead3e774b
Desconden <arda.tekin@yahoo.com>
2022-03-24 22:01:44 +0200

    Proje dosyası ekle.

---
+++ WebProje/Controllers/LoginController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebProje.Models;
+
+namespace WebProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        WareHouseEntities db = new WareHouseEntities();
+        // GET: Login
+        public ActionResult WareHouse()
+        {
+            if (Session["idUser"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LoginPage");
+            }
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginPage(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = db.User.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["idUser"] = data.FirstOrDefault().idUser;
+                    return RedirectToAction("WareHouse");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("LoginPage");
+                }
+            }
+            return View();
+        }
+        //register
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterPage(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.User.FirstOrDefault(s => s.Email == user.Email);
+                if (check == null)
+                {
+                    user.Password = GetMD5(user.Password);
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.User.Add(user);
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    return RedirectToAction("WareHouse");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("LoginPage");
+        }
+
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+    }
+}
---
+++ WebProje/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProje.Models;
+
+namespace WebProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        WareHouseEntities db = new WareHouseEntities();
+        public ActionResult ProductList()
+        {
+            return View(db.Product.ToList());
+        }
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+                db.Configuration.ValidateOnSaveEnabled = false;
+                db.Product.Add(product);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                return RedirectToAction("ProductList");
+            
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Product product = db.Product.SingleOrDefault(s => s.ProductID == id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            ViewBag.message = "Product with name " + product.Name + " Deleted successfully";
+            return View(product);
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product product = db.Product.SingleOrDefault(s => s.ProductID == id);
+
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            Product product_update = db.Product.SingleOrDefault(s => s.ProductID == product.ProductID);
+            product_update.Name = product.Name;
+            product_update.Amount = product.Amount;
+            product_update.ProductType = product.ProductType;
+            db.SaveChanges();
+
+            ViewBag.Message = "Product updated successfully.";
+
+            return View(product_update);
+        }
+    }
+}
